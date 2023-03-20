1775009279bd74f94ccd3361fa2275d8b81cac38
pdushie <philip.dushie@gmail.com>
2023-03-20 15:20:21 +0000

    Add get name, phone number and year of birth

--- getuserdetails.sh
+++ getuserdetails.sh
@@ -3,3 +3,12 @@
 #Date Created: March 20, 2023
 #Date Modified:
 #Description: Script takes user details
+
+echo "Enter your name:"
+read name
+echo "Enter your phone number:"
+read phone
+echo "Enter your year of birth:"
+read yob
+
+echo "Your name is $name,your phone number is $phone, and your current age is $(echo "2023-$yob"|bc)"
